60bd20210498a4f6eba5a1892e1cc61ebfd4716e
Monday_David_Sarka <david_md@hamari.agency>
2023-07-17 00:14:32 +0100

    looped through an array to pull all items

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ import puppeteer from "puppeteer";
 
 (async () => {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://www.google.com/");
-    await page.screenshot({ path: "MyNewPics3.png" });
+    const page = await browser.newPage({ headless: true });
+    await page.goto("https://portal.uniabuja.edu.ng/apply_now.php");
+    // await page.screenshot({ path: "MyNewPics4.png" });
+    const grapPageItem = await page.evaluate(() => {
+        const bTag = document.querySelectorAll(".alert.alert-info a b");
+        // return bTag.innerText;
+        // create an array
+        let programmes = []
+        bTag.forEach((tag) => {
+            programmes.push(tag.innerHTML);
+        })
+        // Return the arrays
+        return programmes;
+    })
+    console.log(grapPageItem)
 
     await browser.close();
 })();
