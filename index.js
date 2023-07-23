d1bdaa05812405b4e4fd8698975f4ef27c630a31
Monday_David_Sarka <david_md@hamari.agency>
2023-07-24 00:13:47 +0100

    Pulled Quotes and Authors names

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ import puppeteer from "puppeteer";
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage({ headless: true });
-    await page.goto("https://portal.uniabuja.edu.ng/apply_now.php");
-    // await page.screenshot({ path: "MyNewPics4.png" });
-    const grapPageItem = await page.evaluate(() => {
-        const bTag = document.querySelectorAll(".alert.alert-info a b");
-        // return bTag.innerText;
-        // create an array
-        let programmes = []
-        bTag.forEach((tag) => {
-            programmes.push(tag.innerHTML);
-        })
-        // Return the arrays
-        return programmes;
-    })
-    console.log(grapPageItem)
+    await page.goto("https://quotes.toscrape.com/");
 
+    const grabQuote = await page.evaluate(() => {
+        const quotes = document.querySelectorAll(".quote")
+
+        let quoteArray = []
+        quotes.forEach((quoteTag) => {
+            const quoteInfo = quoteTag.querySelectorAll('span')
+            const actualQuote = quoteInfo[0]
+            const actualAuthor = quoteInfo[1]
+            const author = actualAuthor.querySelector('small')
+
+            quoteArray.push({quote: actualQuote.innerText, author: author.innerText})
+        })
+        return quoteArray;
+    })
+
+    console.log(grabQuote)
     await browser.close();
 })();
