6520072f68c2087ec941e89bb7eb02523b9d56da
Monday_David_Sarka <david_md@hamari.agency>
2023-07-24 12:23:02 +0100

    Fixed issue with opening browser and autpmated login

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage({ headless: false });
+    // const browser = await puppeteer.launch({dumpio: true});
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome', headless: false});
+    const page = await browser.newPage();
     await page.goto("https://quotes.toscrape.com/");
     await page.click('a[href="/login"]')
 
     await page.type("#username", 'davidowho', {delay: 100})
     await page.type("#password", 'davido25', {delay: 100})
 
-    await page.click('input[value="Logi"]');
+    await page.click('input[value="Login"]');
 
     // await browser.close();
 })();
+
+// ldd: ./chrome: No such file or directory
