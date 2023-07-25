6d6a7d362c7c7da1cf798a4f9600fa4e9c689564
Monday_David_Sarka <david_md@hamari.agency>
2023-07-25 18:27:59 +0100

    Appended data into csv with puppeteer....Tadaagit add .

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
+import csvParser from "csv-parser";
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage({ headless: true });
+  try {
+    const browser = await puppeteer.launch({ executablePath: "/usr/bin/google-chrome", headless: false });
+    const page = await browser.newPage();
     await page.goto("https://quotes.toscrape.com/");
 
     const grabQuote = await page.evaluate(() => {
-        const quotes = document.querySelectorAll(".quote")
+      const quotes = document.querySelectorAll(".quote");
 
-        let quoteArray = []
-        quotes.forEach((quoteTag) => {
-            const quoteInfo = quoteTag.querySelectorAll('span')
-            const actualQuote = quoteInfo[0]
-            const actualAuthor = quoteInfo[1]
-            const author = actualAuthor.querySelector('small')
+      let quoteArray = [];
+      quotes.forEach((quoteTag) => {
+        const quoteInfo = quoteTag.querySelectorAll("span");
+        const actualQuote = quoteInfo[0];
+        const actualAuthor = quoteInfo[1];
+        const author = actualAuthor.querySelector("small");
 
-            quoteArray.push({quote: actualQuote.innerText, author: author.innerText})
-        })
-        return quoteArray;
-    })
+        quoteArray.push({ quote: actualQuote.innerText, author: author.innerText });
+      });
+      return quoteArray;
+    });
 
-    console.log(grabQuote)
+    console.log(grabQuote);
     await browser.close();
+
+    fs.appendFile("data.csv", "", (err) => {
+      if (err) throw err;
+      grabQuote.forEach((item) => {
+        fs.appendFile("data.csv", `${item.quote},${item.author}\n` + "<br />", (err) => {
+          if (err) throw err;
+        });
+      });
+      console.log("Done, thanks for scrapping and embeding into csv file...");
+    });
+  } 
+  catch (err) {
+    console.error("Error occurred:", err);
+  }
+
 })();
